7181e78b31802349e6dcb2281c5c4d7110cbb8da
rewan-95 <rewan36.ra@gmail.com>
2022-07-19 22:35:03 +0300

    First Lesson

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+{{--    <script src="/app.js"></script>--}}
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut deleniti dignissimos doloremque, eos ex id incidunt, minus nemo nesciunt officiis praesentium quia quisquam rem saepe, sed voluptate voluptatem voluptatibus.
+    </p>
+</article>
+
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aut autem commodi dolorem dolorum ducimus enim esse eveniet maxime nobis officiis quas sit veniam? Cumque est odit officiis optio pariatur.
+    </p>
+</article>
+
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur atque beatae cum cupiditate eaque exercitationem explicabo harum in iure libero magnam modi praesentium, ratione voluptates? Asperiores labore nobis repellendus.
+    </p>
+</article>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    return view('post', [
+        'file_content' => file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html")
+    ]);
+});
+
